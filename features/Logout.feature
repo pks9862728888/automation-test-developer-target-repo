e94ad80faceb3164350c4b28d442fa1f49c0c529
pks9862728888 <pks9862728888@gmail.com>
2022-06-25 04:30:56 +0530

    Added parallel run and generation of html reports feature

---
+++ features/Logout.feature
@@ -0,0 +1,5 @@
+Feature: login feature
+
+  Scenario: Login to Admin
+
+    Given Admin user logouts with valid credentials
--- src/test/java/steps/DemoStep.java
+++ src/test/java/steps/DemoStep.java
@@ -1,11 +1,18 @@
 package steps;
 
 import io.cucumber.java.en.Given;
+import org.junit.jupiter.api.Assertions;
 
 public class DemoStep extends BaseTestRunner {
 
     @Given("^Admin user logins with valid credentials$")
     public void adminUserLoginWithValidCredentials() {
-        System.out.println("Login works");
+//        System.out.println("Login works");
+        Assertions.fail("Login failed");
+    }
+
+    @Given("^Admin user logouts with valid credentials$")
+    public void adminUserLogoutWithValidCredentials() {
+//        System.out.println("Logouts works");
     }
 }
